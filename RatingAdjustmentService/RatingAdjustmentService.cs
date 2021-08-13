f83af7b6f330b714f768cee13051111760ed93ba
Harpreet kaur <65912531+hudehyan@users.noreply.github.com>
2021-08-13 16:07:16 -0400

    Add files via upload

--- RatingAdjustmentService/RatingAdjustmentService.cs
+++ RatingAdjustmentService/RatingAdjustmentService.cs
@@ -2,9 +2,7 @@
 
 namespace RatingAdjustment.Services
 {
-    /** Service calculating a star rating accounting for the number of reviews
-     * 
-     */
+   
     public class RatingAdjustmentService
     {
         const double MAX_STARS = 5.0;  // Likert scale
@@ -13,34 +11,31 @@ namespace RatingAdjustment.Services
         double _q;
         double _percent_positive;
 
-        /** Percentage of positive reviews
-         * 
-         * In this case, that means X of 5 ==> percent positive
-         * 
-         * Returns: [0, 1]
-         */
+      
         void SetPercentPositive(double stars)
         {
-            // TODO: Implement this!
+
+            _percent_positive = (stars * 20.0) / 100.0;
+
         }
 
-        /**
-         * Calculate "Q" given the formula in the problem statement
-         */
+      
         void SetQ(double number_of_ratings)
         {
-         // TODO: Implement this!
+            _q = Z * Math.Sqrt(((_percent_positive * (1.0 - _percent_positive)) + ((Z * Z) / (4.0 * number_of_ratings))) / number_of_ratings);
         }
 
-        /** Adjusted lower bound
-         * 
-         * Lower bound of the confidence interval around the star rating.
-         * 
-         * Returns: a double, up to 5
-         */
-        public double Adjust(double stars, double number_of_ratings) {
-            // TODO: Implement this!
-            return 0.0;
+      
+        public double Adjust(double stars, double number_of_ratings)
+        {
+            if (stars <= MAX_STARS)
+            {
+                SetPercentPositive(stars);
+                SetQ(number_of_ratings);
+                double low = ((_percent_positive + ((Z * Z) / (2.0 * number_of_ratings)) - _q) / (1.0 + ((Z * Z) / number_of_ratings)));
+                return (low / 20.0) * 100.0;
+            }
+            return 0.0; 
         }
     }
 }
